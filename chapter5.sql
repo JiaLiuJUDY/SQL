0f15ac67a92b5fda3c303f56750dbd0634af8603
Judy Liu <33583237+JiaLiuJUDY@users.noreply.github.com>
2018-08-10 14:52:15 -0500

    Update chapter5.sql

--- chapter5.sql
+++ chapter5.sql
@@ -1,6 +1,7 @@
-//9.
-Show teamname and the total number of goals scored.
-COUNT and GROUP BY
+// 9.
+// Show teamname and the total number of goals scored.
+// COUNT and GROUP BY
+
 SELECT teamname, COUNT(*)
 FROM eteam JOIN goal ON id=teamid
 GROUP BY teamname
